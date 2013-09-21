f83c83586b5ea2e912b511f4c08bdf7138732789
Trey Simmons <yert511@gmail.com>
2013-09-20 22:25:51 -0500

    Added index.php file - manages controller requests and file inclusions

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+	require 'core/base_classes/loader.php';
+	require 'core/base_classes/base_controller.php';
+	require 'core/base_classes/base_model.php';
+	
+	require 'core/database/db.php';
+	
+	require 'controllers/user_controller.php';
+	require 'models/user_model.php';
+
+	define("APP_NAME","User CRUD");
+	define("BASE_PATH",basename(__DIR__));
+	
+	$loader = new Loader($_GET,$_POST);
+	
+	$controller = $loader->loadController();
+	
+	$controller->executeAction();
+	
