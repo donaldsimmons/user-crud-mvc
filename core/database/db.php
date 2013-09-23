c7852b345bf76851319303b290ac3907326a3604
Trey Simmons <yert511@gmail.com>
2013-09-23 13:37:28 -0500

    Added commenting to db.php

--- core/database/db.php
+++ core/database/db.php
@@ -1,5 +1,7 @@
 <?php
 
+	# establishes constants to be used for loading database in user_model
+	
 	const MY_DSN = "mysql:host=127.0.0.1;port=8889;dbname=user_crud";
 	const MY_USER = "root";
 	const MY_PASS = "root";
